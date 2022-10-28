c4ed84ea41e8b0ff2c5e5c1690500e488705be8e
Zaxyn <ruan.pablo2002@outlook.com.br>
2022-10-28 12:57:48 -0300

    Pedido

---
+++ Ecommerce/src/main/java/com/Loja/Ecommerce/repositories/AdministradorRepository.java
@@ -0,0 +1,9 @@
+package com.Loja.Ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Loja.Ecommerce.models.Administrador;
+
+public interface AdministradorRepository extends JpaRepository<Administrador, Long>{
+
+}
---
+++ Ecommerce/src/main/java/com/Loja/Ecommerce/services/AdministradorService.java
@@ -0,0 +1,59 @@
+package com.Loja.Ecommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.webjars.NotFoundException;
+
+import com.Loja.Ecommerce.models.Administrador;
+import com.Loja.Ecommerce.repositories.AdministradorRepository;
+import com.Loja.Ecommerce.services.exceptions.DataIntegrityException;
+import com.Loja.Ecommerce.services.exceptions.ObjectNotFoundException;
+
+public class AdministradorService {
+	
+	@Autowired
+	AdministradorRepository repository;
+	
+	public List<Administrador> findAll() {
+		return repository.findAll();
+	}
+
+	public Administrador findById(Long id) {
+		try {
+			Administrador administrador = repository.findById(id).get();
+			return administrador;
+		} catch (NotFoundException e) {
+			throw new ObjectNotFoundException("Administrador não encontrado para o id:" + id + ", tipo: " + Administrador.class.getName());
+		}
+	}
+
+	public Administrador inset(Administrador administrador) {
+		administrador.setIdUsuario(null);
+		try {
+			return repository.save(administrador);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Os campos não foram preenchidos corretamente!");
+		}
+	}
+
+	public Administrador update(Administrador administrador) {
+		findById(administrador.getIdUsuario());
+		try {
+			return repository.save(administrador);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Os campos não foram preenchidos corretamente!");
+		}
+	}
+
+	public void delete(Long id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir este administrador!");
+		}
+	}
+
+}
